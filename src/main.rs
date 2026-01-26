a7287821afddcdf216a2f82e8cfea76da6b22664
XicoBM <franciscomarques232@gmail.com>
2026-01-26 18:01:26 +0000

    Define the structure of the server. The server will handle incoming requests and route them to a Microsoft Teams bot.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use axum::{
-    routing::get,
+    routing::{get, post},
+    http::StatusCode,
+    extract::Json,
     Router,
 };
+use tokio::net::TcpListener;
+use serde::Deserialize;
+use serde_json::json;
+use reqwest::Client;
+use std::{env, net::SocketAddr};
+
+#[derive(Deserialize)]
+struct Signal {
+
+}
+
+async fn receive_signal() {}
+
+async fn send_teams_message() {}
 
 
 #[tokio::main]
-async fn main() -> {
-    
+async fn main() {
+    let app = Router::new().route("/", get(|| async {"Hello World"}));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
