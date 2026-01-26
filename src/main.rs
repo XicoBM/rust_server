2d05518488fa4b987bdfa863bd41df6896f47f0c
XicoBM <franciscomarques232@gmail.com>
2026-01-26 18:28:16 +0000

    Do an HTTP handler. This method is supposed to receive an HTTP Signal and respond by invoking the appropriate response function.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,45 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
     extract::Json,
+    http::StatusCode,
+    routing::{get, post},
     Router,
 };
-use tokio::net::TcpListener;
+use reqwest::Client;
 use serde::Deserialize;
 use serde_json::json;
-use reqwest::Client;
 use std::{env, net::SocketAddr};
+use tokio::net::TcpListener;
 
-#[derive(Deserialize)]
+// Expected payload, sent by an ESP32
+#[derive(Deserialize, Debug)]
 struct Signal {
-
+    device_id: String,
+    message: String,
 }
 
-async fn receive_signal() {}
+// HTTP handler, responsible for receiving information in the form of "Signal"
+async fn receive_signal(Json(payload): Json<Signal>) -> Result<StatusCode, StatusCode> {
+    println!(
+        "Received signal from {}: {}",
+        payload.device_id, payload.message
+    );
 
+    let text = format!("Device: {}\n Message: {}", payload.device_id, payload.message); 
+
+    let Err(e) = send_teams_message(&text).await {
+        eprintln!("Error sending message to teams: {e}");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    Ok(StatusCode::Ok)
+}
+
+// Sends message to a Microsoft Teams page through an incoming webhook
 async fn send_teams_message() {}
-
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {"Hello World"}));
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
