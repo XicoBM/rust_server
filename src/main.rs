5752ce10a990a9a183d872433e8073bb7258e609
XicoBM <franciscomarques232@gmail.com>
2026-01-27 12:26:01 +0000

    Develop function to handle the sending of meesages to a Microsoft Teams channel via webhook. Do a simple main function to test the message sending functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use axum::{
     extract::Json,
     http::StatusCode,
-    routing::{get, post},
+    routing::{post},
     Router,
 };
 use reqwest::Client;
 use serde::Deserialize;
 use serde_json::json;
 use std::{env, net::SocketAddr};
-use tokio::net::TcpListener;
 
 // Expected payload, sent by an ESP32
 #[derive(Deserialize, Debug)]
@@ -26,21 +25,35 @@ async fn receive_signal(Json(payload): Json<Signal>) -> Result<StatusCode, Statu
 
     let text = format!("Device: {}\n Message: {}", payload.device_id, payload.message); 
 
-    let Err(e) = send_teams_message(&text).await {
+    if let Err(e) = send_teams_message(&text).await {
         eprintln!("Error sending message to teams: {e}");
         return Err(StatusCode::INTERNAL_SERVER_ERROR);
     }
 
-    Ok(StatusCode::Ok)
+    Ok(StatusCode::OK)
 }
 
 // Sends message to a Microsoft Teams page through an incoming webhook
-async fn send_teams_message() {}
+async fn send_teams_message(text: &str) -> Result<(), reqwest::Error> {
+    let webhook_url = env::var("TEAMS_WEBHOOK_URL").expect("TEAMS_WEBHOOK_URL not set up");
+
+    let payload = json!({
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "summary": "New Event",
+        "themeColor": "0076D7",
+        "title": "Event Received",
+        "text": text
+    });
+
+    Client::new().post(webhook_url).json(&payload).send().await?.error_for_status()?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World" }));
-
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    send_teams_message("Teste direto do Rust 🦀")
+        .await
+        .unwrap();
 }
