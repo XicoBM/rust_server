202b57feb89d71eadd57e90c75362aba413cdc3a
XicoBM <franciscomarques232@gmail.com>
2026-03-20 16:38:55 +0000

    Make big alterations to the project structure:  - Move all source files to a new `src` directory.  - Update the `Cargo.toml` file to reflect the new structure.  - Change the method used for handling the passing of info to the server, switching from command-line arguments to environment variables for better security and flexibility.  - Update the `.gitignore` file to exclude any new files or directories that should not be tracked by Git, such as build artifacts or sensitive configuration files.  - Refactor the function and method used fot transmiting the info received from the ESP32.  - Ensure that all changes are properly documented in the code and commit messages for future reference.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use axum::{
     extract::Json,
     http::StatusCode,
-    routing::{post},
+    routing::get,
     Router,
 };
-use reqwest::Client;
+use lettre::transport::smtp::client::{Tls, TlsParameters};
+use lettre::{
+    message::Mailbox, transport::smtp::authentication::Credentials, Message, SmtpTransport,
+    Transport,
+};
 use serde::Deserialize;
-use serde_json::json;
-use std::{env, net::SocketAddr};
+use std::env;
+use dotenvy::dotenv;
+use local_ip_address::local_ip;
 
 // Expected payload, sent by an ESP32
 #[derive(Deserialize, Debug)]
@@ -23,9 +28,12 @@ async fn receive_signal(Json(payload): Json<Signal>) -> Result<StatusCode, Statu
         payload.device_id, payload.message
     );
 
-    let text = format!("Device: {}\n Message: {}", payload.device_id, payload.message); 
+    let text = format!(
+        "Device: {}\n Message: {}",
+        payload.device_id, payload.message
+    );
 
-    if let Err(e) = send_teams_message(&text).await {
+    if let Err(e) = send_email_alert(&text).await {
         eprintln!("Error sending message to teams: {e}");
         return Err(StatusCode::INTERNAL_SERVER_ERROR);
     }
@@ -33,27 +41,86 @@ async fn receive_signal(Json(payload): Json<Signal>) -> Result<StatusCode, Statu
     Ok(StatusCode::OK)
 }
 
-// Sends message to a Microsoft Teams page through an incoming webhook
-async fn send_teams_message(text: &str) -> Result<(), reqwest::Error> {
-    let webhook_url = env::var("TEAMS_WEBHOOK_URL").expect("TEAMS_WEBHOOK_URL not set up");
+// Function responsible for the sending of emails
+async fn send_email_alert(body: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let body = body.to_string();
 
-    let payload = json!({
-        "@type": "MessageCard",
-        "@context": "http://schema.org/extensions",
-        "summary": "New Event",
-        "themeColor": "0076D7",
-        "title": "Event Received",
-        "text": text
-    });
+    let result = tokio::task::spawn_blocking(move || {
+        let credentials_one = env::var("EMAIL_USER").expect("EMAIL_USER not set");
+        let credentials_two = env::var("EMAIL_PASSWORD").expect("EMAIL_PASSWORD not correct");
+        let credentials_three =env::var("EMAIL_RECEIVER").expect("EMAIL_RECEIVER not set up");
 
-    Client::new().post(webhook_url).json(&payload).send().await?.error_for_status()?;
+        let email = Message::builder()
+            .from(Mailbox::new(
+                Some("Servidor Rust".into()),
+                credentials_one.parse()?,
+            ))
+            .to(credentials_three.parse()?)
+            .subject("Alerta do sistema")
+            .body(body)?;
+
+        let creds = Credentials::new(credentials_one.to_string(), credentials_two.to_string());
+
+        let tls_parameters = TlsParameters::new("smtp.gmail.com".to_string())?;
+
+        let mailer = SmtpTransport::relay("smtp.gmail.com")?
+            .port(587)
+            .tls(Tls::Required(tls_parameters))
+            .credentials(creds)
+            .build();
+
+        mailer.send(&email)?;
+        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+    })
+    .await?;
+
+    result?;
 
     Ok(())
 }
 
+// This function serves as a simple way for testing/simulating the ESP32 signal
+async fn signal_testing() -> Result<StatusCode, StatusCode> {
+    let device = "huawei_pc";
+    let message = "testing again";
+
+    let text = format!("Device: {}\n Message: {}", device, message);
+
+    if let Err(e) = send_email_alert(&text).await {
+        eprintln!("Error sending message to teams: {e}");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    Ok(StatusCode::OK)
+}
+
 #[tokio::main]
 async fn main() {
-    send_teams_message("Teste direto do Rust 🦀")
-        .await
-        .unwrap();
+    dotenv().ok();
+    let app = Router::new()
+        .route("/", get(|| async { "Testing" }))
+        .route("/signal", get(signal_testing).post(receive_signal));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:12345").await.unwrap();
+
+    match local_ip() {
+        Ok(ip) => println!("Server running on {:?}:12345", ip),
+        Err(_e) => eprintln!("Error obtaining the local IP address.")
+    }
+
+    axum::serve(listener, app).await.unwrap();
 }
+
+
+/**
+ * Main problems with this code:
+ *  1. Both routes defined are unprotected and/or exposed:
+ *      1.1  In the /signal endpoint there is no authentication, so it is possible to uncover the IP and port in use;
+ *           This could be solved by implementing a static token in the header of every request.
+ * 
+ *      1.2 In the same endpoint, the get is also exposed without authentication.
+ * 
+ *  2. The payload is not limited. Since Axum accepts, by defect, big bodies, it is possible to send a JSON to big for the program.
+ * 
+ *  3. The data in the payload that is received is sent to the email without treatment, so special characters are going to be passed along.      
+ */
